b36bc3c17f3d868db748b64deee1f177346b56a2
ellen sierens <ellen.sierens@student.howest.be>
2020-06-13 15:06:36 +0200

    added gps in J5

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var socket = require('socket.io-client')('https://evening-caverns-60077.herokuapp.com/');
-const { Board, Servo, Motors } = require("johnny-five");
+const { Board, Servo, Motors, GPS } = require("johnny-five");
 let servo;
 let motors;
+let gps;
 const invertPWM = true;
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
@@ -22,6 +23,24 @@ board.on("ready", function () {
     { pins: { dir: 5, pwm: 6 }, invertPWM },
     { pins: { dir: 12, pwm: 11 }, invertPWM },
   ]);
+
+  gps = new GPS({
+    pins: {
+      rx: 11,
+      tx: 9,
+    }
+  });
+
+  // If latitude, longitude change log it
+   gps.on("change", position => {
+    const {altitude, latitude, longitude} = position;
+    console.log("GPS Position:");
+    console.log("  latitude   : ", position.latitude);
+    console.log("  longitude  : ", position.longitude);
+    console.log("  altitude   : ", position.altitude);
+    console.log("--------------------------------------");
+  });
+
   console.log("ready");
 });
 
