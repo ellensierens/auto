378edbaf31984e224a7c55831f52e2ae60a7f440
ellen sierens <ellen.sierens@student.howest.be>
2020-06-13 15:24:31 +0200

    test zonder gps met standardFirmataPlus

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var socket = require('socket.io-client')('https://evening-caverns-60077.herokuap
 const { Board, Servo, Motors, GPS } = require("johnny-five");
 let servo;
 let motors;
-let gps;
+// let gps;
 const invertPWM = true;
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
@@ -24,22 +24,22 @@ board.on("ready", function () {
     { pins: { dir: 12, pwm: 11 }, invertPWM },
   ]);
 
-  gps = new GPS({
-    pins: {
-      rx: 11,
-      tx: 10,
-    }
-  });
+  // gps = new GPS({
+  //   pins: {
+  //     rx: 11,
+  //     tx: 10,
+  //   }
+  // });
 
     // If latitude, longitude change log it
-    gps.on("change", position => {
-      const {altitude, latitude, longitude} = position;
-      console.log("GPS Position:");
-      console.log("  latitude   : ", position.latitude);
-      console.log("  longitude  : ", position.longitude);
-      console.log("  altitude   : ", position.altitude);
-      console.log("--------------------------------------");
-    });
+    // gps.on("change", position => {
+    //   const {altitude, latitude, longitude} = position;
+    //   console.log("GPS Position:");
+    //   console.log("  latitude   : ", position.latitude);
+    //   console.log("  longitude  : ", position.longitude);
+    //   console.log("  altitude   : ", position.altitude);
+    //   console.log("--------------------------------------");
+    // });
 
   console.log("ready");
 });
